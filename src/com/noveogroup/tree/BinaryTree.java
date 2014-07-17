aad6a8416d07b41ee573b768a3e13915a6c3aaae
Sergey Bragin <sergeybragin@rocketmail.com>
2014-07-17 10:00:08 +0700

    Implement addition and removal of an element
    Implement addition and removal of an element in the binary tree. Iterator
    and exceptions to be implemanted.

--- src/com/noveogroup/tree/BinaryTree.java
+++ src/com/noveogroup/tree/BinaryTree.java
@@ -1,6 +1,7 @@
 package com.noveogroup.tree;
 
 import com.noveogroup.exception.BinaryTreeException;
+import com.noveogroup.model.TreeElement;
 
 import java.util.Iterator;
 
@@ -8,7 +9,7 @@ import java.util.Iterator;
  * Binary tree interface. Please add your exceptions to methods.
  */
 public interface BinaryTree <K,V> {
-    void addElement(K key, V element) throws BinaryTreeException;
+    void addElement(K key, V item) throws BinaryTreeException;
     void removeElement(K key) throws BinaryTreeException;
-    Iterator<V> getIterator();
+    Iterator<TreeElement<K, V>> getIterator();
 }
--- src/com/noveogroup/tree/BinaryTreeImpl.java
+++ src/com/noveogroup/tree/BinaryTreeImpl.java
@@ -1,26 +1,102 @@
 package com.noveogroup.tree;
 
+import com.noveogroup.exception.BinaryTreeException;
+import com.noveogroup.model.TreeElement;
+import com.noveogroup.model.TreeElementImpl;
+import com.noveogroup.model.TreeTuple;
+import com.noveogroup.item.TreeItem;
+
 import java.util.Iterator;
 
 /**
  * Sample implementation.
  */
-public class BinaryTreeImpl<K,V> implements BinaryTree<K,V> {
+public class BinaryTreeImpl<K extends Comparable<? super K>, V extends TreeItem> implements BinaryTree <K, V> {
+    TreeElement<K, V> root;
 
     @Override
-    public void addElement(K key, V element) {
-        //TODO
+    public void addElement(K key, V item) throws BinaryTreeException {
+        TreeElementImpl<K, V> element = new TreeElementImpl<K, V>(key, item);
+        addElement(element, null);
     }
 
     @Override
-    public void removeElement(K key) {
-        //TODO
+    public void removeElement(K key) throws BinaryTreeException {
+        TreeTuple<K, V> tuple = search(key, null);
+
+        if(tuple.child == null) {
+            System.out.println("Here an exception throw should be.");
+        }
+        else {
+            if(tuple.child.getChild(false) != null) {
+                putElement(tuple.child.getChild(false), tuple.parent, tuple.is_big);
+                if(tuple.child.getChild(true) != null) {
+                    addElement(tuple.child.getChild(true), tuple.child.getChild(false));
+                }
+            }
+            else {
+                if(tuple.child.getChild(true) != null) {
+                    putElement(tuple.child.getChild(true), tuple.parent, tuple.is_big);
+                }
+                else {
+                    putElement(null, tuple.parent, tuple.is_big);
+                }
+            }
+        }
     }
 
     @Override
-    public Iterator<V> getIterator() {
+    public Iterator<TreeElement<K, V>> getIterator() {
         //TODO
         return null;
     }
 
+    protected void putElement(TreeElement<K, V> child, TreeElement<K, V> parent, boolean is_big) {
+        if(child != null) {
+            child.setParent(parent);
+        }
+        if(parent != null) {
+            parent.setChild(child, is_big);
+        }
+        else {
+            root = child;
+        }
+    }
+
+    protected void addElement(TreeElement<K, V> element, TreeElement<K, V> starting_element) throws BinaryTreeException {
+        TreeTuple<K, V> tuple = search(element.getKey(), starting_element);
+        if(tuple.child != null) {
+            System.out.println("Here an exception throw should be.");
+        }
+        else {
+            putElement(element, tuple.parent, tuple.is_big);
+        }
+    }
+
+    protected TreeTuple<K, V> search(K key, TreeElement<K, V> starting_element) {
+        if(starting_element == null) {
+            starting_element = root;
+        }
+        if(starting_element == null) {
+            return (new TreeTuple<K, V>(null, null, false));
+        }
+
+        TreeElement <K, V> temp = starting_element;
+        TreeElement <K, V> temp_parent = temp.getParent();
+        boolean is_big = (temp_parent.getChild(true) == temp);
+
+        while(true) {
+            if(temp == null || temp.getKey().compareTo(key) == 0) {
+                return (new TreeTuple<K, V>(temp, temp_parent, is_big));
+            }
+            if(temp.getKey().compareTo(key) > 0) {
+                is_big = false;
+            }
+            if(temp.getKey().compareTo(key) < 0) {
+                is_big = true;
+            }
+            temp_parent = temp;
+            temp = temp.getChild(is_big);
+        }
+    }
 }
